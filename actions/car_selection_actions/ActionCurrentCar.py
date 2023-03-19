e646853c0bda5f3473e8120621ce2f4dec09f99c
phonix007 <telegramtutorial007@gmail.com>
2023-03-19 09:20:20 +0530

    car selection coding done

---
+++ actions/car_selection_actions/ActionCurrentCar.py
@@ -0,0 +1,25 @@
+from typing import Dict, Text, Any, List, Union
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionCurrentCar(Action):
+    def name(self) -> Text:
+        return "action_current_car"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # get the currently selected car from tracker
+        current_car = tracker.get_slot("selected_car")
+
+        # generate a response to inform the user about the currently selected car
+        if current_car is not None:
+            message = f"Your current car is '{current_car}'."
+        else:
+            message = "You haven't selected a car yet."
+
+        # send the message to the user
+        dispatcher.utter_message(message)
+
+        return []
